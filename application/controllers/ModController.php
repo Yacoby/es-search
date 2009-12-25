196c643ec48702e601dc92e5a9f09938e4fe64f5
Jacob@JacobEssex.com <Jacob@JacobEssex.com@f2546742-f0b6-11de-ba40-05aa5ec858d5>
2009-12-25 11:24:05 +0000

    Added mod details redirect if the name paramater is wrong
    git-svn-id: http://es-search.googlecode.com/svn/trunk@11 f2546742-f0b6-11de-ba40-05aa5ec858d5

--- application/controllers/ModController.php
+++ application/controllers/ModController.php
@@ -19,16 +19,47 @@
  * l-b */
 
 class ModController extends Zend_Controller_Action {
-    
+
+    protected function nameToUrlName($str) {
+        return str_replace(
+                ' ',
+                '-',
+                preg_replace('/[^a-zA-Z0-9 ]/', '', (string)$str)
+        );
+
+    }
+
     public function detailsAction() {
         $request = $this->getRequest();
-        $id = $request->getParam("id", "-1");
 
-        if ( !is_numeric($id) || $id == -1 ) {
+        $id = $request->getParam("id", -1);
+        if ( !is_numeric($id) || $id === -1 ) {
             throw new Exception("Wrong mod id");
         }
 
+
         $mod = new Default_Model_Mod($id);
+
+        $urlReqName = $request->getParam('name', '');
+        $urlActName = $this->nameToUrlName($mod->getName());
+
+        if ( $urlActName != $urlReqName ) {
+
+            $goto = array(
+                    'controller'    =>'mod',
+                    'action'        => 'details',
+                    'id'            => $id,
+                    'name'          => $urlActName
+            );
+
+            $redirector = $this->_helper->getHelper('Redirector');
+            $redirector->setCode(301)
+                    ->gotoRoute($goto, 'mod')
+                    ->redirectAndExit();
+        }
+
+
+
 
         $this->view->Name = $this->view->title = $mod->getName();
         $this->view->Author = $mod->getAuthor();
