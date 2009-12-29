7523ad927b7941866c7060251822f59ffac656d7
Jacob@JacobEssex.com <Jacob@JacobEssex.com@f2546742-f0b6-11de-ba40-05aa5ec858d5>
2009-12-29 17:13:41 +0000

    Cache now works
    git-svn-id: http://es-search.googlecode.com/svn/trunk@34 f2546742-f0b6-11de-ba40-05aa5ec858d5

--- public/index.php
+++ public/index.php
@@ -28,22 +28,37 @@ require "Zend/Cache.php";
 $lifeTimeMult = APPLICATION_ENV == 'production' ? 60 : 0;
 
 $frontendOptions = array(
-        'lifetime' => 5*$lifeTimeMult,
-        'automatic_cleaning_factor' => 100,
-        //@todo change regex to store the first page of results.
+        'lifetime'                          => 10*$lifeTimeMult,
+        'automatic_cleaning_factor'         => 100,
+        'cache_with_get_variables'          => true,
+        'cache_with_cookie_variables'       => true,
+        'cache_with_session_variables'      => true,
+        'make_id_with_session_variables'    => false,
+
+        //I suspect there is a bug somewhere for this to be required
+        'default_options' => array(
+                'lifetime'                          => 10*$lifeTimeMult,
+                'automatic_cleaning_factor'         => 100,
+                'cache_with_get_variables'          => true,
+                'cache_with_cookie_variables'       => true,
+                'cache_with_session_variables'      => true,
+                'make_id_with_session_variables'    => false
+        ),
+
         'regexps' => array(
-                '^/search\\?' => array('cache' => false)
+                '/search\?'            => array('cache' => false),
+                '/search\?.*page=1'    => array('cache' => true),
         )
+
 );
 
 $cache = Zend_Cache::factory(
         'Page',
         'File',
         $frontendOptions,
-        array('cache_dir' => '../cache/page/')
+        array('cache_dir' => ROOT_PATH.'/cache/page')
 );
 $cache->start();
-
 
 $application = createApplication();
 $application->run();
