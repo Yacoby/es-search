d0c3cd6f9da948497b5bec71eb34859f66f1c91d
jacob@JacobEssex.com <jacob@JacobEssex.com@f2546742-f0b6-11de-ba40-05aa5ec858d5>
2011-01-15 14:51:08 +0000

    Fixed Major issue in mod Search_Table_Mod::findMatch. Cleanup of code.
    git-svn-id: http://es-search.googlecode.com/svn/trunk@42 f2546742-f0b6-11de-ba40-05aa5ec858d5

--- library/Search/Table/Mods.php
+++ library/Search/Table/Mods.php
@@ -25,6 +25,43 @@ class Search_Table_Mods extends Search_Table_Abstract {
         return $text;
     }
 
+    private function getModId($name, $author, $url){
+        $match = $this->findMatch( $name,
+                                   $author,
+                                   $url);
+        return $match ? $match->id : null;
+    }
+    private function getGameId($game){
+        $gameId = Doctrine_Query::create()
+                    ->select('g.id')
+                    ->from('Game g')
+                    ->where('g.short_name = ?', $game)
+                    ->fetchOne(array(), Doctrine_Core::HYDRATE_SINGLE_SCALAR);
+
+        //Convert to better expcetion class
+        if ( $gameId === false ){
+            throw new Exception('No Valid Game');
+        }
+        return $gameId;
+    }
+
+    private function getCategoryId($category){
+        $categories = new Search_Table_Categories();
+        $category   = $categories->createQuery('c')
+                                  ->select('c.id')
+                                  ->where('c.name = ?', $category)
+                                  ->fetchOne(array(),
+                                             Doctrine_Core::HYDRATE_SINGLE_SCALAR);
+
+        if ( $category === false ){
+            $category = $categories->create();
+            $category->name = $modDetails['Category'];
+            $category->save();
+            return $category->id;
+        }
+        return $category;
+    }
+
     /**
      * @todo this is horrible :(
      *
@@ -34,28 +71,18 @@ class Search_Table_Mods extends Search_Table_Abstract {
     public function addOrUpdateModFromArray(Search_Table_Sites $sites, array $modDetails){
         $this->getConnection()->beginTransaction();
 
-        $match = $this->findMatch( $modDetails['Name'],
-                                   $modDetails['Author'],
-                                   $modDetails['Url']);
-        $modId = $match ? $match->id : null;
+        $modId = $this->getModId($modDetails['Name'],
+                                 $modDetails['Author'],
+                                 $modDetails['Url']);
 
         foreach ( array('Name', 'Author', 'Category', 'Description') as $key ){
             $modDetails[$key] = $this->stripText($modDetails[$key]);
         }
 
-        $modDetails['Game'] = strtolower($modDetails['Game']);
+        $modDetails['Game']     = strtolower($modDetails['Game']);
         $modDetails['Category'] = strtolower($modDetails['Category']);
 
-        $gameId = Doctrine_Query::create()
-                    ->select('g.id')
-                    ->from('Game g')
-                    ->where('g.short_name = ?', $modDetails['Game'])
-                    ->fetchOne(array(), Doctrine_Core::HYDRATE_SINGLE_SCALAR);
-
-        //Convert to better expcetion class
-        if ( $gameId === false ){
-            throw new Exception('No Valid Game');
-        }
+        $gameId = $this->getGameId($modDetails['Game']);
 
         $mod = $modId ? $this->findOneById($modId) : $this->create();
         $mod->name           = $modDetails['Name'];
@@ -70,32 +97,24 @@ class Search_Table_Mods extends Search_Table_Abstract {
         $url  = $modDetails['Url'];
         $site = $sites->findOneByHost($url->getHost());
 
-        $modUrlSuffix = substr((string)$url, strlen($site->base_url . $site->mod_url_prefix));
+        $modUrlSuffix = substr((string)$url,
+                               strlen($site->base_url . $site->mod_url_prefix));
 
-        //TODO Recks unit testing
+        
         $locations = new Search_Table_Locations();
         $location = $locations->create();
 
-        $categories = new Search_Table_Categories();
-        $category = $categories->createQuery('c')
-                    ->select('c.id')
-                    ->where('c.name = ?', $modDetails['Category'])
-                    ->fetchOne(array(), Doctrine_Core::HYDRATE_SINGLE_SCALAR);
-        if ( $category === false ){
-            $category = $categories->create();
-            $category->name = $modDetails['Category'];
-            $category->save();
-            $category = $category->id;
-        }
+
+        $categoryId = $this->getCategoryId($modDetails['Category']);
 
         $location->mod_url_suffix   = $modUrlSuffix;
-        $location->version     = $modDetails['Version'];
-        $location->description = $modDetails['Description'];
+        $location->version          = $modDetails['Version'];
+        $location->description      = $modDetails['Description'];
 
-        $location->category_id    = $category;
+        $location->category_id      = $categoryId;
 
-        $location->modification_id      = $mod->id;
-        $location->site_id     = $site->id;
+        $location->modification_id  = $mod->id;
+        $location->site_id          = $site->id;
 
         $location->replace();
 
@@ -126,7 +145,9 @@ class Search_Table_Mods extends Search_Table_Abstract {
                             ->from('Modification m')
                             ->leftJoin('m.Locations l')
                             ->leftJoin('l.Site s')
-                            ->where('CONCAT(s.base_url, s.mod_url_prefix, l.mod_url_suffix) = ?', (string)$url)
+                            ->where('CONCAT(s.base_url,
+                                            s.mod_url_prefix,
+                                            l.mod_url_suffix) = ?', (string)$url)
                             ->fetchOne();
 
         if ( $record !== false ){
@@ -137,7 +158,7 @@ class Search_Table_Mods extends Search_Table_Abstract {
         $record = $this->createQuery()
                         ->select()
                         ->where('name=?', $name)
-                        ->where('author=?', $author)
+                        ->andWhere('author=?', $author)
                         ->fetchOne();
 
         return $record !== false ? $record : null;
