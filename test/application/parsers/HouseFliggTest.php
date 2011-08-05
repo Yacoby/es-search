09b3233f2dd47e58377b8d60ad8cfe91c04ac86a
Jacob Essex <jacob@jacobessex.com>
2011-08-05 20:53:56 +0100

    More work on house fligg parser

---
+++ test/application/parsers/HouseFliggTest.php
@@ -0,0 +1,27 @@
+<?php
+
+class HouseFliggTest{
+    public function testMods(){
+        new 
+
+        $factory = new Search_Parser_Factory(APPLICATION_PATH . '/parsers/defaults.ini',
+                                                    APPLICATION_PATH . '/parsers/parsers.ini');
+
+    }
+
+    private function modsContain($mods, $needle){
+        foreach ( $mods as $mod ){
+            $found = true;
+            foreach ( $needle as $key => $value ){
+                if ( $mod[$key] != $value ){
+                    $flag = false;
+                    break;
+                }
+            }
+            if ( $found ){
+                return true;
+            }
+        }
+        return false;
+    }
+}
