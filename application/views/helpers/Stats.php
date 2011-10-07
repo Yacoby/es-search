ed74a03a66fc33b7f8472ee186e9fd584ff2441c
Jacob Essex <jacob@jacobessex.com>
2011-10-07 21:33:25 +0100

    Fixed the mod count stats on the main page

--- application/views/helpers/Stats.php
+++ application/views/helpers/Stats.php
@@ -2,15 +2,24 @@
 
 class Zend_View_Helper_Stats{
     public function stats(){
-        $rows = Doctrine_Query::create()
-                    ->select('game_id, COUNT(*) as count')
-                    ->from('Modification')
-                    ->groupBy('game_id')
-                    ->execute(Doctrine::HYDRATE_ARRAY);
+        //I didn't manage to get this working using Doctrine correctly, so
+        //I gave up and used raw sql
+        $sql = 'SELECT
+                    game_id, COUNT(*) as count
+                FROM modification
+                GROUP BY game_id';
+
+        $dbh =  Doctrine_Manager::getInstance()
+                                ->getCurrentConnection()
+                                ->getDbh();
+
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         $games = array(3 => 0, 4 => 0, 5 => 0);
         foreach ( $rows as $row ){
-            $games[$row->game_id] = $row->count;
+            $games[$row['game_id']] = $row['count'];
         }
 
         $modCount = Doctrine_Query::create()
